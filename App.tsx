2bf9badd12d9699593a425f451d5d0bb5e241611
Yuri Nascimento <yurinascimennto@gmail.com>
2024-09-22 13:52:49 -0300

    #F059, usando expo-fonts junto com useFonts para personalizar fonts no app

--- App.tsx
+++ App.tsx
@@ -1,7 +1,14 @@
+import { ActivityIndicator } from "react-native";
 import { Groups } from "./src/screens/Groups";
 
+
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+
 export default function App() {
+
+  const [fontsLoaded, error] = useFonts({Roboto_400Regular, Roboto_700Bold})
+  
   return (
-    <Groups />
+     fontsLoaded ? <Groups/> : <ActivityIndicator/>
   );
 }
