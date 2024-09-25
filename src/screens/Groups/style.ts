cb9cdbf5a6fa6574ff0ccb4c65241af3bf392f1a
Yuri Nascimento <yurinascimennto@gmail.com>
2024-09-25 12:26:56 -0300

    setando o padding de Groups para 24 com fim de evitar que os componentes fiquem muito na borda

--- src/screens/Groups/style.ts
+++ src/screens/Groups/style.ts
@@ -5,6 +5,7 @@ import { theme } from "../../theme";
 export const styles = StyleSheet.create({
     container: {
       flex: 1,
+      padding: 24,
       backgroundColor: theme.COLORS.GRAY_600,
-    },
+    }
   });
